abafb22fcc9ef3a310eaf293604cdcd84442534e
Axelcxg333 <axelcxg333@gmail.com>
2025-04-23 23:06:55 -0500

    refactor: Modificacion del header
    Se modifico el header cambiando el titulo principal, añadiendo el eslogan ya modificado y se comento el boton innecesario

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,27 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx(styles.heroBanner)}>
       <div className="container">
+        <img src='img/image.png' style={{width: 150}}></img>
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Bienvenido a {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          {/*
           <Link
-            className="button button--secondary button--lg"
+            className="button button--primary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ver Guias 🎓
           </Link>
+          */}
         </div>
       </div>
     </header>
@@ -32,8 +34,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Inicio"
+      description="">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
